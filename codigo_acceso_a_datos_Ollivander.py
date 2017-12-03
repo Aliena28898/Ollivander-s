da5f552b4ffd978e5a122292cefb3b7d62714b49
Unknown <crisgonmon@hotmail.com>
2017-12-03 21:34:17 +0100

    first refactoring
    first refactoring

--- codigo_acceso_a_datos_Ollivander.py
+++ codigo_acceso_a_datos_Ollivander.py
@@ -1,26 +1,41 @@
+"""Estos dos primeros métiodos van a permitir abrir un fichero de texto y extraer de él los casos test en forma de
+matriz de 3 dimensiones [Primera dimensión: lista de los atributos del ítem. Segunda dimensión: lista de ítems de un día.
+Tercera dimensión: lista de listas de cada día. Los casos test están escritos en la siguiente forma:
 
+-------- día X --------
+atributo1, atributo2, atributo3 (Nombre del. Ejemplo: Nombre, Caducidad, Calidad)
+atributo1, atributo2, atributo3 (del primer ítem)
+atributo1, atributo2, atributo3 (del segundo ítem y sucesivamente)
+[*SALTO DE LÍNEA*]
+---------día X+1--------
+[...]
 
-def accesoCasosTexttest(matrizCasosTest, rutaAccesoFichero):
+"""
 
+def abrirFichero(rutaAccesoFichero):
     try:
         if not isinstance(rutaAccesoFichero, str):
             raise ValueError
-        fichero = open(rutaAccesoFichero, 'r')
+        return open(rutaAccesoFichero, 'r')
     except FileNotFoundError:
         print("Fichero no encontrado")
         return []
     except ValueError:
         print("El nombre del fichero ha de ser un string")
         return []
-    else:
+
+def accesoCasosTexttest(matrizCasosTest, rutaAccesoFichero):
+    fichero = abrirFichero(rutaAccesoFichero)
+
+    if fichero != []:
         matrizCasosTest = []
         numeroPropiedadesItem = 0
         for linea in fichero:
-            if linea.find("day") != -1:
+            if "day" in linea:
                 casosTestDia = []
             elif linea == "\n":
                 matrizCasosTest.append(casosTestDia)
-            elif linea.find("name") != -1:
+            elif "name" in linea:
                 numeroPropiedadesItem = len(linea.split(','))
             else:
                 item = linea.rstrip().rsplit(',', maxsplit=numeroPropiedadesItem - 1)
